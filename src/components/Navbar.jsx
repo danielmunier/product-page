528df9e76c645755dc7ab56e231508a824f8a8e4
Daniel <dmunier27@gmail.com>
2024-06-04 18:08:55 -0300

    responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from "../assets/images/logo.svg"
 import cart from "../assets/images/icon-cart.svg"
 import avatar from "../assets/images/image-avatar.png"
+import closeMenu from "../assets/images/icon-close.svg"
 import "./navbar.css"
 
 
 const Navbar = () => { 
+  const [active, setActive] = useState(false)
 
+  const handleOpenMenu = (e) => {
+    setActive(!active)
+  }
+
+ 
 
   return (
-      <header>
-        <img src={logo} alt="logo" />
-          <ul>
+      <nav>
+        <div className="mobile-menu" onClick={handleOpenMenu}>
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
+        <img src={logo} alt="Main Logo" />
+
+       
+
+          <ul className={`nav-list ${active ? "active" : ""}`}>
+
+            <div className='close-menu' onClick={handleOpenMenu}>
+              <img src={closeMenu} alt="Close menu" />
+            </div>
+
             <li><a href="">Collections</a></li>
             <li><a href="">Men</a></li>
             <li><a href="">Women</a></li>
@@ -19,11 +39,11 @@ const Navbar = () => {
             <li><a href="">Contact</a></li>
           </ul>
 
-        <div className='cartAvatar'>
-          <img id='cart-button' src={cart} alt="Cart" />
-          <img id='avatar-button' src={avatar} alt="" srcset="" />
+        <div className='cart-avatar'>
+          <button><img id='cart-button' src={cart} alt="Cart" /></button>
+          <button><img id='avatar-button' src={avatar} alt="" srcset="" /></button>
         </div>
-      </header>
+      </nav>
   )
   
 }
